31b1cd87308c94dceabdb52e1e93f6fff28895f5
VZhyrytskiy <Vitaliy_Zhyrytskyy@epam.com>
2025-03-10 09:16:14 +0200

    feat: non string props

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
 import './App.css'
 import Contact from './components/Contact'
 
+type Person = {
+  name: string;
+  phone: string;
+  email: string;
+  isAvailable: boolean;
+  age: number;
+};
+
+
 function App() {
+  const person: Person = {
+    name: "Vitaliy",
+    phone: "+38 067 000 00 00",
+    email: "v.zhiritskiy@gmail.com",
+    isAvailable: true,
+    age: 30
+  };
+
   return (
     <>
-      <Contact
-        name="Vitaliy"
-        phone="+38 067 000 00 00"
-        email="v.zhiritskiy@gmail.com"
-        />
-      <Contact
-        name="Anna"
-        phone="+38 067 111 00 00"
-        email="a.zhiritskaya@gmail.com"
-        />      
+      <Contact person={person} />
     </>
   )
 }
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,22 +4,31 @@
 //     email: string;
 // }
 
-type ContactProps = {
+type Person = {
   name: string;
   phone: string;
   email: string;
+  isAvailable: boolean;
+  age: number;
+};
+
+type ContactProps = {
+  person: Person;
 };
 
 
 const Contact: React.FC<ContactProps> = (props) => {
-  console.log(props);  
+  console.log(props);
+  const { name, phone, email, isAvailable, age } = props.person;  
   return (
     <>
         <h2>Contact</h2>
         <section>
-            <p>Name: {props.name}</p>
-            <p>Phone: {props.phone}</p>
-            <p>Email: {props.email}</p>
+            <p>Name: {name}</p>
+            <p>Phone: {phone}</p>
+            <p>Email: {email}</p>
+            <p>isAvailable: {isAvailable.toString()}</p>
+            <p>Age: {age}</p>
         </section>
     </>
   )
